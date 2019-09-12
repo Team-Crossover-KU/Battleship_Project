5bc5d136f88f6a064cf33b5ecb247e17661e797f
Jack McClure <jack_mcclure@ku.edu>
2019-09-12 14:04:46 -0500

    Feature: Boards and Buttons
    BIG commit. Added a user interactive board, and a smaller board to see where the enemy has hit.

---
+++ Battleship/Assets/Scripts/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public int hitOrMiss = 1; //0 for a miss, 1 for a hit
+    public Sprite[] impactIcons;
+    public Button[] spacesAvailable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Setup();
+    }
+
+    void Setup()
+    {
+        for(int i = 0; i < spacesAvailable.Length; i++)
+        {
+            spacesAvailable[i].interactable = true;
+            spacesAvailable[i].GetComponent<Image>().sprite = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BattleshipSpaces(int num)
+    {
+        spacesAvailable[num].image.sprite = impactIcons[hitOrMiss];
+        if(hitOrMiss == 0)
+        {
+            hitOrMiss = 1;
+        }
+        else
+        {
+            hitOrMiss = 0;
+        }
+        spacesAvailable[num].interactable = false;
+    }
+}
